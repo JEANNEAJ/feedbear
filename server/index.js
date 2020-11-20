fc75d72e6106bdebd2c00691f66ae6e8dcf304e2
Nathan Kanigsberg <nathan@kanigsberg.com>
2020-11-20 11:03:57 -0500

    initial commit

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
+
+app.get('/', (req, res) => {
+	res.send('Hello world');
+})
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => app.listen(PORT, ( ) => console.log(`Server running on port: ${PORT}`)))
+	.catch(err=> console.log(err));
