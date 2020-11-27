4abbad5a7f70201ecb45cd639a796ccc99fc0cd4
Nathan Kanigsberg <nathan@kanigsberg.com>
2020-11-27 16:30:51 -0500

    add authentication server-side

---
+++ server/models/users.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const userSchema = mongoose.Schema({
+  name: String,
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+// hashing the user password to be stored safely
+userSchema.pre(
+  'save',
+  async function (next) {
+    const user = this;
+    const hash = await bcrypt.hash(user.password, 10);
+    user.password = hash;
+    next();
+  }
+);
+
+userSchema.methods.isValidPassword = async function (password) {
+  const user = this;
+  const compare = await bcrypt.compare(password, user.password);
+  console.log('did login comparison:', compare)
+  return compare;
+}
+
+const User = mongoose.model('User', userSchema);
+
+
+
+export default User;
