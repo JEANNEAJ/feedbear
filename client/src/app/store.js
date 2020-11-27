a7243f5ce3e5976fc30156066bdec5dea2d9c2b5
Evan Wallace <evan.wallace@mail.utoronto.com>
2020-11-27 15:51:31 -0500

    create userSlice and handle login actions

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 // import counterReducer from '../features/counter/counterSlice';
 import formReducer from '../features/form/formSlice';
+import userReducer from '../features/signIn/userSlice';
 
 export default configureStore({
   reducer: {
 		// counter: counterReducer,
-		form: formReducer,
+    form: formReducer,
+    user: userReducer
   },
 });
