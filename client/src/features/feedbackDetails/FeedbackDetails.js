578d7e963b37476bf68344e414fa336c38efb430
Evan Wallace <evan.wallace@mail.utoronto.com>
2020-11-28 08:40:04 -0500

    fix async useEffect in FeedbackDetails

--- client/src/features/feedbackDetails/FeedbackDetails.js
+++ client/src/features/feedbackDetails/FeedbackDetails.js
@@ -12,17 +12,18 @@ export default function FeedbackDetails(props) {
 	const { name, email, message, projectLink, liveLink, createdAt } = request;
 
 
-	useEffect(async () => {
-		try {
-			const { data } = await api.fetchFormByID(feedbackID);
-			
-			console.log(data);
-
-			setRequest(data[0]);
-		} catch (error) {
-			console.error(error);
+	useEffect(() => {
+		const populateRequests = async () => {
+			try {
+				const { data } = await api.fetchFormByID(feedbackID);
+				
+				setRequest(data[0]);
+			} catch (error) {
+				console.error(error);
+			}
 		}
-	}, []);
+		populateRequests();
+	}, [feedbackID]);
 	
 	
 	return (
@@ -33,7 +34,7 @@ export default function FeedbackDetails(props) {
 				<>
 					<h2>Project Name</h2>
 					<p>by {name}</p>
-					<img src="https://placekitten.com/200/300" alt="Placeholder image"/>
+					<img src="https://placekitten.com/200/300" alt="Placeholder"/>
 					<a href={liveLink}>View App</a>
 					<a href={projectLink}>View Repository</a>
 
