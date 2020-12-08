987c757322718a82ad6bf8102b80cd1c2758b8b3
Joey Chau <joeymchau@gmail.com>
2020-12-08 10:34:27 -0500

    finish update functionality

--- client/src/features/feedbackRequest/FeedbackRequestForm.js
+++ client/src/features/feedbackRequest/FeedbackRequestForm.js
@@ -1,32 +1,57 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { submit } from "./feedbackRequestSlice";
+import { useHistory } from "react-router-dom";
+
+import { submit, update } from "./feedbackRequestSlice";
 import { selectUser } from "../user/userSlice";
 
 import styles from "./FeedbackRequestForm.module.css";
 
-export default function FeedbackRequestForm() {
+export default function FeedbackRequestForm({ inputText, requestId }) {
   const user = useSelector(selectUser);
   const { _id: userId, name } = user;
   const [message, setMessage] = useState("Tear me to shreds!");
   const [projectTitle, setProjectTitle] = useState("");
   const [projectLink, setProjectLink] = useState("");
   const [liveLink, setLiveLink] = useState("");
+
   const dispatch = useDispatch();
+  const history = useHistory();
+
+  useEffect(() => {
+    if (inputText) {
+      setProjectTitle(inputText.projectTitle);
+      setProjectLink(inputText.projectLink);
+      setLiveLink(inputText.liveLink);
+      setMessage(inputText.message);
+    }
+  }, [])
 
   // submit the form data
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(
-      submit({
-        userId,
-        name,
-        message,
-        projectTitle,
-        projectLink,
-        liveLink,
-      })
-    );
+    if (inputText) {
+      dispatch(
+        update({
+          message,
+          projectTitle,
+          projectLink,
+          liveLink,
+        }, requestId)
+      )
+      history.push('/user/:userId');
+    } else {
+      dispatch(
+        submit({
+          userId,
+          name,
+          message,
+          projectTitle,
+          projectLink,
+          liveLink,
+        })
+      );
+    }
   };
 
   return (
--- client/src/features/feedbackRequest/UpdateRequest.js
+++ client/src/features/feedbackRequest/UpdateRequest.js
@@ -1,11 +1,29 @@
 import React, { useEffect, useState } from 'react'
 import FeedbackRequestForm from './FeedbackRequestForm';
+import { useRouteMatch } from 'react-router-dom';
+import * as api from "../../api/forms";
+
 export const UpdateRequest = () => {
+
+  const { params: { requestId } } = useRouteMatch();
+  const [request, setRequest] = useState('');
+
+  useEffect(() => {
+    const getSavedRequest = async () => {
+      try {
+        const { data } = await api.fetchFormByID("_id", requestId);
+        setRequest(data[0]);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getSavedRequest();
+  }, [])
 
   return (
     <div>
       <h3>Edit Your Requests:</h3>
-      {request ? <FeedbackRequestForm /> : <p>Loading ...</p>}
+      {request ? <FeedbackRequestForm inputText={request} requestId={requestId} /> : <p>Loading ...</p>}
     </div>
   )
 }
