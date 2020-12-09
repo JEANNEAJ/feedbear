6e3863ffb2c1b0e2c355b30f8baf8ddda023a9fe
Evan Wallace <evanpatrickwallace@gmail.com>
2020-12-09 17:42:47 -0500

    implement logout route on the server

--- server/routes/session.js
+++ server/routes/session.js
@@ -1,8 +1,9 @@
-import express from 'express';
-import { checkLoggedIn } from '../controllers/session.js';
+import express from "express";
+import { checkLoggedIn, logout } from "../controllers/session.js";
 
 const router = express.Router();
 
-router.get('/session', checkLoggedIn);
+router.get("/session", checkLoggedIn);
+router.delete("/session", logout);
 
 export default router;
