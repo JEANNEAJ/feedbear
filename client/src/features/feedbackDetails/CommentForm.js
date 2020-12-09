f953859c3f20ad7dc5d7f48d616e6b68f1afa55d
Evan Wallace <evanpatrickwallace@gmail.com>
2020-12-09 15:17:06 -0500

    style FeedbackDetails

--- client/src/features/feedbackDetails/CommentForm.js
+++ client/src/features/feedbackDetails/CommentForm.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from "react";
 
 export default function CommentForm() {
   return (
     <>
       <label className="sr-only" htmlFor="input-feedback">
         Your feedback
-            </label>
+      </label>
       <textarea
+        className="input-text"
         name="input-feedback"
         id="input-feedback"
         placeholder="Leave your feedback"
       ></textarea>
 
-      <button type="submit">Submit</button>
+      <button className="btn-submit" type="submit">
+        Submit
+      </button>
     </>
-  )
+  );
 }
--- client/src/features/feedbackDetails/FeedbackDetails.js
+++ client/src/features/feedbackDetails/FeedbackDetails.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-import Form from '../../components/form/Form';
+import Form from "../../components/form/Form";
 
 import * as api from "../../api/forms";
 
@@ -11,7 +11,14 @@ export default function FeedbackDetails(props) {
 
   const [request, setRequest] = useState([]);
   // console.log(request);
-  const { name, message, projectTitle, projectLink, liveLink, createdAt } = request;
+  const {
+    name,
+    message,
+    projectTitle,
+    projectLink,
+    liveLink,
+    createdAt,
+  } = request;
 
   useEffect(() => {
     const populateRequests = async () => {
@@ -28,23 +35,28 @@ export default function FeedbackDetails(props) {
 
   return (
     <div>
-      <p>ID: {feedbackID}</p>
-
       {!request ? (
         "Loading..."
       ) : (
         <>
-          <h2>{projectTitle}</h2>
+          <h2 className="text-xl font-bold">{projectTitle}</h2>
           <p>by {name}</p>
           <p>created at {createdAt}</p>
-          <img src="https://placekitten.com/200/300" alt="Placeholder" />
           <a href={liveLink}>View App</a>
           <a href={projectLink}>View Repository</a>
-          <p>{message}</p>
+          <img
+            className="mx-auto max-w-full my-3"
+            src="https://placekitten.com/400/300"
+            alt="Placeholder"
+          />
+          <p className="mb-10">{message}</p>
 
+          <h3 className="text-xl font-bold">Feedback</h3>
           <Form type="CommentForm" />
         </>
       )}
+
+      <p className="text-sm">ID: {feedbackID}</p>
     </div>
   );
 }
