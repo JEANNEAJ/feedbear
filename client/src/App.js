14983a74e3efdc8b89896c8b0e293d39c57ef0ea
Evan Wallace <evanpatrickwallace@gmail.com>
2020-12-14 16:08:35 -0500

    separate App routing into AuthorizedApp and UnauthorizedApp

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,25 @@
 import React, { useEffect } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
+import { selectUser, checkForUserSession } from "./features/user/userSlice";
 
-import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
+import Nav from "./components/nav/Nav";
+import AuthenticatedApp from "./AuthenticatedApp";
+import UnauthenticatedApp from "./UnauthenticatedApp";
+import Footer from "./components/Footer/Footer";
 
 import "./App.css";
 
-import FeedbackDetails from "./features/feedbackDetails/FeedbackDetails";
-
-import LoginForm from "./features/user/LoginForm";
-import { selectUser, checkForUserSession } from "./features/user/userSlice";
-import SignupForm from "./features/user/SignupForm";
-import UserPage from "./features/user/UserPage";
-
-import Form from './components/form/Form';
-
-import Nav from "./components/nav/Nav";
-import Dashboard from "./components/Dashboard/Dashboard";
-import Footer from './components/Footer/Footer';
-
-import { UpdateRequest } from "./features/feedbackRequest/UpdateRequest";
-import { AuthRoute, ProtectedRoute } from './util/route';
-
-
 function App() {
   const user = useSelector(selectUser);
-  const userSessionChecked = useSelector(state => state.user.userSessionChecked);
+  const userSessionChecked = useSelector(
+    (state) => state.user.userSessionChecked
+  );
   const dispatch = useDispatch();
 
-  useEffect(() => { dispatch(checkForUserSession()) }, []);
+  useEffect(() => {
+    dispatch(checkForUserSession());
+  }, [dispatch]);
 
   return (
     <Router>
@@ -40,22 +32,15 @@ function App() {
 
         <main>
           <div className="wrapper">
-            <Switch>
-              <Route exact path="/">
-                { 
-                  userSessionChecked 
-                  ? user._id ? <Dashboard /> : <Form type="LoginForm" />
-                  : <p>Loading</p>  
-                }
-              </Route>
-              
-              <AuthRoute path="/signup" component={Form} type="SignupForm" />
-            
-              <ProtectedRoute exact path="/feedback/:feedbackID" component={FeedbackDetails} />
-              <ProtectedRoute exact path="/user/:userId" component={UserPage} />
-              <ProtectedRoute exact path="/edit/:requestId" component={UpdateRequest} />
-              <Redirect to="/" />
-            </Switch>
+            {userSessionChecked ? (
+              user._id ? (
+                <AuthenticatedApp />
+              ) : (
+                <UnauthenticatedApp />
+              )
+            ) : (
+              <p>Loading</p>
+            )}
           </div>
         </main>
 
