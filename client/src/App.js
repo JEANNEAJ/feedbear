80aa708ec49d5c444420cc9589380189c4ec3c0c
Andre <drefacey@gmail.com>
2020-12-16 16:15:00 -0500

    merge split authenticated app and checking for logged in user on initial load

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,6 @@
-<<<<<<< HEAD
 import React from "react";
-import { useSelector } from "react-redux";
-
-import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
-
-import "./App.css";
-
-import FeedbackDetails from "./features/feedbackDetails/FeedbackDetails";
-
-import UserPage from "./features/user/UserPage";
-
-import Form from './components/form/Form';
-=======
-import React, { useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { selectUser, checkForUserSession } from "./features/user/userSlice";
->>>>>>> master
+import { useSelector } from "react-redux";
 
 import Nav from "./components/nav/Nav";
 import AuthenticatedApp from "./AuthenticatedApp";
@@ -25,18 +9,8 @@ import Footer from "./components/Footer/Footer";
 
   import "./tailwind.output.css";
   
-  function App() {
-    const user = useSelector(selectUser);
-    const isLoggedIn = useSelector(state => state.user.isLoggedIn);
-  const userSessionChecked = useSelector(
-    (state) => state.user.userSessionChecked
-  );
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(checkForUserSession());
-  }, [dispatch]);
-
+function App() {
+  const isLoggedIn = useSelector(state => state.user.isLoggedIn);
 
   return (
     <Router>
@@ -49,16 +23,13 @@ import Footer from "./components/Footer/Footer";
 
         {/* main component fills space b/w header and footer, centered vertically */}
         <main className="flex-grow flex flex-col justify-around p-5">
-          {userSessionChecked ? (
-            user._id ? (
+          {
+            isLoggedIn ? (
               <AuthenticatedApp />
             ) : (
               <UnauthenticatedApp />
             )
-          ) : (
-            <p>Loading</p>
-          )}
-
+          }
         </main>
 
         <Footer />
