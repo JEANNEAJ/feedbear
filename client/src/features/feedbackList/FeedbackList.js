bfe0ca541e3e975ce5b0311b167464ae782dadfa
Nathan Kanigsberg <nathan@kanigsberg.com>
2020-12-20 14:15:38 -0500

    Test infinite scroll with empty data

--- client/src/features/feedbackList/FeedbackList.js
+++ client/src/features/feedbackList/FeedbackList.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
+import InfiniteScroll from 'react-infinite-scroll-component';
+
 import FeedbackListItem from "./FeedbackListItem";
 import * as api from "../../api/forms";
 
 export default function FeedbackList() {
   const [requests, setRequests] = useState([]);
+  const [items, setItems] = useState(Array.from({ length: 20 }));
+  // const [requestsToDisplay, setRequestsToDisplay] = useState([]);
 
   useEffect(() => {
     handleRefresh();
@@ -13,10 +17,17 @@ export default function FeedbackList() {
     try {
       const { data } = await api.fetchForms();
       setRequests(data);
+      
     } catch (error) {
       console.log(error);
     }
   };
+
+  const fetchNext = () => {
+    setTimeout(() => {
+      setItems(items.concat(Array.from({ length: 20 })))
+    }, 1500);
+  }
 
   return (
     <div className="container mx-auto">
@@ -24,11 +35,36 @@ export default function FeedbackList() {
         <h2 className="text-xl font-bold">Feedback Requests</h2>
         <button onClick={handleRefresh}>refresh 🔃</button>
       </div>
+
       <ul>
-        {requests.map((request) => (
-          <FeedbackListItem key={request._id} request={request} />
-        ))}
+        <InfiniteScroll
+          dataLength={items.length} //This is important field to render the next data
+          next={fetchNext}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}
+          endMessage={
+            <p style={{ textAlign: 'center' }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
+          // below props only if you need pull down functionality
+          // refreshFunction={this.refresh}
+          // pullDownToRefresh
+          // pullDownToRefreshThreshold={50}
+          // pullDownToRefreshContent={
+          //   <h3 style={{ textAlign: 'center' }}>&#8595; Pull down to refresh</h3>
+          // }
+          // releaseToRefreshContent={
+          //   <h3 style={{ textAlign: 'center' }}>&#8593; Release to refresh</h3>
+          // }
+        >
+          {items.map((item,index)=><p>item {index}</p>)}
+          {/* {!requests.length ? 'Nothing here' : requests.map((request) => (
+            <FeedbackListItem key={request._id} request={request} />
+          ))} */}
+        </InfiniteScroll>
       </ul>
+
     </div>
   );
 }
