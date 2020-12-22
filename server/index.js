aa91e172f5760e8c8fb0c76d73c2b134c63bb4dd
Evan Wallace <evanpatrickwallace@gmail.com>
2020-12-22 17:08:09 -0500

    remove unused secureRoutes

--- server/index.js
+++ server/index.js
@@ -6,16 +6,15 @@ import dotenv from "dotenv";
 import passport from "passport";
 import "./strategies/strategies.js";
 
-import session from 'express-session';
-import connectStore from "connect-mongo"
+import session from "express-session";
+import connectStore from "connect-mongo";
 
 // routes
 import formRoutes from "./routes/forms.js";
 import authRoutes from "./routes/auth.js";
-import secureRoutes from "./routes/secure-routes.js";
 import sessionRoutes from "./routes/session.js";
-import commentRoutes from './routes/comments.js';
-import userRoutes from './routes/user.js';
+import commentRoutes from "./routes/comments.js";
+import userRoutes from "./routes/user.js";
 
 // settings
 mongoose.Promise = global.Promise;
@@ -37,7 +36,6 @@ mongoose
 mongoose.set("useFindAndModify", false);
 mongoose.set("useCreateIndex", true);
 
-
 // middleware stack
 const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
@@ -57,28 +55,20 @@ app.use(session({
   }),
   cookie: {
     sameSite: true,
-    secure: process.env.NODE_ENV === 'production', // when set to true the cookie will only work on HTTPS
-    maxAge: parseInt(process.env.SESS_LIFETIME)
-  }
-}));
+      secure: process.env.NODE_ENV === "production", // when set to true the cookie will only work on HTTPS
+      maxAge: parseInt(process.env.SESS_LIFETIME),
+    },
+  })
+);
 
 app.use("/forms", formRoutes);
 app.use(authRoutes);
-app.use(secureRoutes);
 app.use(sessionRoutes);
 app.use(commentRoutes);
 app.use(userRoutes);
-
-// TODO: decide if we want to use this approach for private routes
-app.use(
-  "/user",
-  passport.authenticate("jwt", { session: false }),
-  secureRoutes
-);
 
 // error-handling middleware
 app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.json({ message: err.message || "An unidentified error occurred." });
 });
-
