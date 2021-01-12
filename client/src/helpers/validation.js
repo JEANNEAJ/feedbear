36584bde7bcbde5fb6c886928c3c3671cc902607
Joey Chau <joeymchau@gmail.com>
2021-01-12 14:27:55 -0500

    fix validator rules for pssw

--- client/src/helpers/validation.js
+++ client/src/helpers/validation.js
@@ -13,4 +13,4 @@ export function formatToUrl(str) {
 export const validateEmail = email => validator.isEmail(email);
 
 // default settings: minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1
-export const validatePassword = pssw => validator.isStrongPassword(pssw, { minLength: 10, returnScore: false })
+export const validatePassword = pssw => validator.isStrongPassword(pssw, { minSymbols: 0 })
