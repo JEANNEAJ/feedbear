fdb2431fb8649db0c46ae459f9bd623ff647e5c8
Andre <drefacey@gmail.com>
2021-01-13 20:19:27 -0500

    add check to verify owner of project before rendering edit form

--- client/src/components/pages/UpdateProject.js
+++ client/src/components/pages/UpdateProject.js
@@ -1,34 +1,53 @@
 import React, { useEffect, useState } from "react";
 import ProjectForm from "../forms/ProjectForm";
-import { useRouteMatch } from "react-router-dom";
+import { useRouteMatch, Redirect } from "react-router-dom";
 import * as projectApi from "../../api/projects";
+import { selectUser } from "../../slices/userSlice";
+import { useSelector } from "react-redux";
 
 export const UpdateProject = () => {
   const {
     params: { projectId },
   } = useRouteMatch();
+  
+  const [validUser, setValidUser] = useState({check: false, valid: false})
   const [project, setProject] = useState("");
 
+  const currentUser = useSelector(selectUser)
+
   useEffect(() => {
-    const getSavedRequest = async () => {
-      try {
-        const { data } = await projectApi.fetchProjectByID("_id", projectId);
-        setProject(data[0]);
-      } catch (error) {
-        console.error(error);
-      }
-    };
     getSavedRequest();
+    
   }, []);
+  
+  const getSavedRequest = async () => {
+    try {
+      const { data } = await projectApi.fetchProjectByID("_id", projectId);
+
+      if (data[0]?.userId === currentUser._id) {
+        setProject(data[0]);
+        setValidUser({check: true, valid: true})
+      } else {
+        setValidUser({check: true, valid: false})
+      }
+      
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  if (validUser.check === false) {
+    return <h2>Loading...</h2>
+  }
+
+  if (validUser.check && validUser.valid === false) {
+    return <Redirect to="/" />
+  }
 
   return (
     <div>
       <h3>Edit Your Requests:</h3>
-      {project ? (
-        <ProjectForm buttonText="Save" values={project} projectId={projectId} />
-      ) : (
-        <p>Loading ...</p>
-      )}
+      <ProjectForm buttonText="Save" values={project} projectId={projectId} />
     </div>
   );
 };
