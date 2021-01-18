fefaa77d7de470072ec91803f691344837d55b4f
Joey Chau <joeymchau@gmail.com>
2021-01-17 20:45:28 -0500

    if user exists, auto-fill input

--- client/src/components/forms/ContactForm.js
+++ client/src/components/forms/ContactForm.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { useSelector } from "react-redux";
 
 export default function ContactForm() {
+
+  const user = useSelector((state) => state.user);
+  const { _id, name } = user.data;
+
   return (
-    <form className='form' onSubmit=''>
-      <input name='contactName' type='text' placeholder='Name' />
-      <input name='contactEmail' type='email' placeholder='Email' />
+    <form className='form text-center' onSubmit=''>
+      {!_id ? (
+        <>
+          <input name='contactName' type='text' placeholder='Name' />
+          <input name='contactEmail' type='email' placeholder='Email' />
+        </>
+      ) : (
+          <input name='contactName' type='text' value={name} className='text-gray-400' disabled />
+        )
+      }
       <textarea name='contactMsg' placeholder='Enter Message Here!'></textarea>
+      <button className='btn-submit'>Send</button>
     </form>
   )
 }
