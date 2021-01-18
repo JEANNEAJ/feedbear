dbad3cd7b5225ed09870a6c8850ececc7ed31fdf
Joey Chau <joeymchau@gmail.com>
2021-01-17 21:03:22 -0500

    add link to project details page for app

--- client/src/components/pages/ContactPage.js
+++ client/src/components/pages/ContactPage.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import ContactForm from './../forms/ContactForm';
 
 export default function ContactPage() {
   return (
     <>
-      <h1>Contact Us</h1>
+      <h2>Contact Us</h2>
+      <p>Have some comments about our app? Mention it on our very own <Link to={'/project/6004d12085b0123a7889a88a'}>feedback request!</Link></p>
+
       <ContactForm />
     </>
   )
