b2140188a9913865dde1b3feb6e4b05c357a7553
Joey Chau <joeymchau@gmail.com>
2021-01-20 20:45:06 -0500

    add Swal modal to indicate successfull send

--- client/src/components/forms/ContactForm.js
+++ client/src/components/forms/ContactForm.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import { useSelector } from "react-redux";
+import Swal from 'sweetalert2';
 
 export default function ContactForm() {
 
   const user = useSelector((state) => state.user);
   const { _id, name } = user.data;
 
+  const onSubmit = () => {
+    Swal.fire({
+      title: 'Message Sent!',
+      text: 'Sent Successfully',
+      icon: 'success'
+    }).then(() => reset())
+  }
+
   return (
-    <form className='form text-center' onSubmit=''>
-      {!_id ? (
+    <form className='form text-center' onSubmit={onSubmit}>
+      {!_id ?
         <>
           <input name='contactName' type='text' placeholder='Name' />
           <input name='contactEmail' type='email' placeholder='Email' />
         </>
-      ) : (
-          <input name='contactName' type='text' value={name} className='text-gray-400' disabled />
-        )
+        :
+        <input name='contactName' type='text' value={name} className='text-gray-400' disabled />
       }
-      <textarea name='contactMsg' placeholder='Enter Message Here!'></textarea>
-      <button className='btn-submit'>Send</button>
+      <button className='btn-submit' type='submit'>Send</button>
     </form>
   )
 }
