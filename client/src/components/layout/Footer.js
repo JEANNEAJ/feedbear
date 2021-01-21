430dccafee97e38f7cb33409f3ac707b6ac0ec18
Joey Chau <joeymchau@gmail.com>
2021-01-20 20:16:28 -0500

    add link to contact page in footer

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,13 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
     <footer className="text-white text-center">
       <div className="container mx-auto p-5">
         <p>
-          Created by the graduates of Juno College
+          Created by the <Link to={'/contact'}>graduates</Link> of Juno College
         </p>
-        <p>©2020</p>
+        <p>© 2020</p>
       </div>
     </footer>
   );
