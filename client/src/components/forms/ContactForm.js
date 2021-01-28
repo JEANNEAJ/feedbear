53ea9b4091faf509b9734c7ff87339e13ee3abf3
Joey Chau <joeymchau@gmail.com>
2021-01-28 12:06:00 -0500

    connect contact form to formspree

--- client/src/components/forms/ContactForm.js
+++ client/src/components/forms/ContactForm.js
@@ -3,15 +3,22 @@ import { useSelector } from "react-redux";
 import { useForm } from "react-hook-form";
 import { validateEmail } from "../../helpers/validation";
 import Swal from 'sweetalert2';
+import Axios from 'axios';
 
 export default function ContactForm() {
 
   const user = useSelector((state) => state.user);
-  const { _id, name } = user.data;
+  const { _id, name, email } = user.data;
 
   const { register, handleSubmit, errors, reset } = useForm();
 
-  const onSubmit = () => {
+  const onSubmit = (data) => {
+    if (_id) data.email = email;
+    Axios({
+      url: process.env.REACT_APP_EMAIL_URL,
+      method: 'POST',
+      data
+    })
     Swal.fire({
       title: 'Message Sent!',
       text: 'Sent Successfully',
@@ -31,26 +38,31 @@ export default function ContactForm() {
           {errors.contactName && <span>Name cannot be empty</span>}
 
           <input type='email'
-            name='contactEmail'
+            name='email'
             placeholder='Email'
             ref={register({
               required: true,
               validate: value => validateEmail(value)
             })}
           />
-          {errors.contactEmail?.type === 'required' && <span>E-mail cannot be empty</span>}
-          {errors.contactEmail?.type === 'validate' && <span>Please enter a valid e-mail</span>}
+          {errors.email?.type === 'required' && <span>E-mail cannot be empty</span>}
+          {errors.email?.type === 'validate' && <span>Please enter a valid e-mail</span>}
 
         </>
         :
-        <input name='contactName' type='text' value={name} className='text-gray-400' ref={register({ required: true })} disabled />
+        <input type='text'
+          name='contactName'
+          value={name}
+          className='text-gray-400'
+          ref={register}
+        />
       }
       <textarea
-        name='contactMsg'
+        name='message'
         placeholder='Enter Message Here!'
         ref={register({ required: true })}
       ></textarea>
-      {errors.contactMsg && <span>Message cannot be empty</span>}
+      {errors.message && <span>Message cannot be empty</span>}
 
       <button className='btn-submit' type='submit'>Send</button>
     </form>
