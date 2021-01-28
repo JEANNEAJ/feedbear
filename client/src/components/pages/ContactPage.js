8430479136f54b90a12e3d8adf891299ba99944c
Joey Chau <joeymchau@gmail.com>
2021-01-28 11:59:38 -0500

    pull contributor info from github api

--- client/src/components/pages/ContactPage.js
+++ client/src/components/pages/ContactPage.js
@@ -1,39 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
 import ContactForm from './../forms/ContactForm';
+import Axios from 'axios';
 
 export default function ContactPage() {
 
-  const dustySticks = [
-    {
-      name: 'Evan Wallace',
-      githubUser: 'epwallace'
-    },
-    {
-      name: 'Nathan Kanigsberg',
-      githubUser: 'nkanigsberg'
-    },
-    {
-      name: 'Andre Facey',
-      githubUser: 'afacey'
-    },
-    {
-      name: 'Joey Chau',
-      githubUser: 'j-chau'
-    },
-  ]
+  const [dustySticks, setDustySticks] = useState([])
+
+  useEffect(() => {
+    Axios({
+      url: 'https://api.github.com/repos/jeanneaj/feedbear/contributors',
+      method: 'GET',
+      dataResponse: 'json'
+    }).then(res => setDustySticks(res.data))
+  }, [])
+
   return (
     <>
       <h2>Contact Us</h2>
-      <p>Have some comments about our app? Mention it on our very own <Link to={'/project/6004d12085b0123a7889a88a'}>feedback request!</Link></p>
+      <p>Have some comments about our app? Mention it on our very own <Link to={'/project/5ff63422551e1e10ac0a44a1'}>feedback request!</Link></p>
 
       <div>
         <h3>Contributors</h3>
         <ol>
           {dustySticks.map(contributor => {
             return (
-              <li>
-                <a href={`https://github.com/${contributor.githubUser}`} target="_blank" rel="noopener noreferrer">{contributor.name}</a>
+              <li key={contributor.id}>
+                <a href={contributor.html_url} target="_blank" rel="noopener noreferrer">{contributor.login}</a>
               </li>
             )
           })}
