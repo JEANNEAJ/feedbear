4b3dc15904ff3fe378aab71a2929dd67ead51f0d
Evan Wallace <evanpatrickwallace@gmail.com>
2021-02-03 15:16:11 -0500

    revised project card layout

--- client/src/components/projects/Project.js
+++ client/src/components/projects/Project.js
@@ -15,63 +15,73 @@ export default function Project(props) {
     createdAt,
     _id,
     commentsCount,
+    file,
   } = props.project;
 
   return (
     <li className="bg-white rounded-lg shadow-sm mt-3 px-3 py-2 hover:bg-gray-100">
+      {/* project header */}
+      <div className="flex justify-between w-full flex-shrink-0">
+        {/* posted by {user} at {time difference} */}
+        <div className="flex items-center">
+          <img
+            className="object-cover w-5 h-5 mr-1 rounded-full inline-block"
+            src={userId?.avatar ? userId.avatar : bear}
+            alt={`${name}'s avatar`}
+          />
+          <p className="text-sm">
+            <Link to={{ pathname: `/user/${userId}`, name }}>{name}</Link>{" "}
+            <TimeDifference dateString={createdAt} /> ago
+          </p>
+        </div>
+
+        {/* demo and repo links */}
+        <div className="flex items-end text-sm space-x-2 mt-auto">
+          {_id !== "5ff63422551e1e10ac0a44a1" && ( //omit links from pinned project
+            <>
+              <a href={liveLink} target="_blank" rel="noopener noreferrer">
+                Demo
+              </a>
+              <a href={projectLink} target="_blank" rel="noopener noreferrer">
+                Code
+              </a>
+            </>
+          )}
+        </div>
+      </div>
+
+      {/* project body */}
       <div className="flex justify-between">
-        <div>
-          {/* row flex for avatar + project metadata/links */}
-          <div className="flex items-center">
-            {/* user avatar */}
-            <img
-              className="object-cover h-16 w-16 rounded-full my-auto"
-              src={userId?.avatar ? userId.avatar : bear}
-              alt={`${name}'s avatar`}
-            />
-
-            {/* col flex for project metadata/links */}
-            <div className="flex flex-col ml-2">
-              {/* project title */}
-              <h3 className="text-lg">
-                <Link to={`/project/${_id}`}>{projectTitle}</Link>
-              </h3>
-
-              {/* project info + links */}
-              <p>
-                submitted by{" "}
-                <Link to={{ pathname: `/user/${userId}`, name }}>{name}</Link>{" "}
-                <TimeDifference dateString={createdAt} /> ago
-              </p>
-
-              {_id !== "5ff63422551e1e10ac0a44a1" && ( //omit links from pinned project
-                <div className="flex space-x-2">
-                  <a href={liveLink} target="_blank" rel="noopener noreferrer">
-                    View App
-                  </a>
-                  <a
-                    href={projectLink}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    View Repository
-                  </a>
-                </div>
-              )}
-            </div>
-          </div>
+        {/* LEFT SIDE: title, message, and comment count */}
+        <div className="flex flex-col">
+          {/* project title */}
+          <h3 className="text-xl mt-3">
+            <Link to={`/project/${_id}`}>{projectTitle}</Link>
+          </h3>
 
           {/* project message */}
-          <p className="mt-5">{message}</p>
-        </div>
-        <div className="flex flex-col justify-between items-end w-24">
-          {/* Comment count */}
-          <Link to={`/project/${_id}`} title="Number of comments">
-            💬{commentsCount || 0}
-          </Link>
+          <p>{message}</p>
 
-          {/* edit and delete button from UserPage */}
-          {props.children}
+          <div className="flex items-end text-sm space-x-2 mt-auto pt-5">
+            {/* Comment count */}
+            <Link to={`/project/${_id}`} title="Number of comments">
+              💬{commentsCount || 0} comments
+            </Link>
+          </div>
+        </div>
+
+        {/* RIGHT SIDE: image thumbnail, edit/delete buttons */}
+        <div className="pl-5 flex-shrink-0 flex flex-col justify-between w-60 items-end">
+          {file && (
+            <img
+              className="object-cover rounded-lg mt-10"
+              src={file}
+              alt={`Preview for ${name}'s project, "`}
+            />
+          )}
+
+          {/* edit and delete button (if applicable) */}
+          <div className="mt-auto">{props.children}</div>
         </div>
       </div>
     </li>
