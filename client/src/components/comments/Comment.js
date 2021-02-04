e4cf5ef1ef0223022c1f7af0bbd529ecdde4a3e3
Evan Wallace <evanpatrickwallace@gmail.com>
2021-02-04 12:09:55 -0500

    resize avatar in comments

--- client/src/components/comments/Comment.js
+++ client/src/components/comments/Comment.js
@@ -68,26 +68,20 @@ export default function Comment(props) {
         "Loading..."
       ) : (
         <>
-          {/* row flex for avatar + comment metadata */}
-          <div className="flex mb-3 items-center">
+          {/* posted by {user} at {time difference} */}
+          <div className="flex items-center mb-3">
             <img
-              className="object-cover w-12 h-12 rounded-full"
+              className="object-cover w-5 h-5 mr-1 rounded-full inline-block"
               src={avatar ? avatar : bear}
-              alt={name}
+              alt={`${name}'s avatar`}
             />
-
-            {/* col flex for comment metadata */}
-            <div className="pl-3 flex flex-col">
-              <h4 className="font-bold">
-                <Link to={`/user/${userId}`}>
-                  {name}
-                  {isUserComment() && " (You)"}
-                </Link>
-              </h4>
-              <p className="text-sm">
-                submitted <TimeDifference dateString={createdAt} /> ago
-              </p>
-            </div>
+            <p className="text-sm">
+              <Link to={{ pathname: `/user/${userData?._id}`, name }}>
+                {name}
+                {isUserComment() && " (You)"}
+              </Link>{" "}
+              <TimeDifference dateString={createdAt} /> ago
+            </p>
 
             {/* edit/delete button (if applicable) */}
             {isUserComment() && (
--- client/src/components/pages/ContactPage.js
+++ client/src/components/pages/ContactPage.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import ContactForm from "./../forms/ContactForm";
 import Axios from "axios";
 
